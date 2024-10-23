f73aa6a2508400ecceee8c5900c8154b9b8aaaaa
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2024-10-24 00:49:46 +0530

    clear comment

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const baseURL = "https://api.thecatapi.com/v1/images/search?api_key=YOUR_API_KEY./image";
 const img =document.querySelector("#image");
 const btn = document.querySelector("button");
-
 const getData = async ()=>{
-    console.log("getting data .....");
+    // console.log("getting data .....");
     let response = await fetch(baseURL);
-    console.log(response);
+    // console.log(response);
     let importData = await response.json();
-    console.log(importData[0]);
+    // console.log(importData[0]);
     img.src = importData[0].url;
-
 }
-
 btn.addEventListener("click", getData);
